4cc0ecae0d72ba038ae0b7911e3b6d9d222f9ad9
Mason Wan <mwan@linkedin.com>
2017-05-09 16:32:38 -0700

    Make `gulp watch` works

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,42 @@
 const gulp = require('gulp')
+const mocha = require('gulp-mocha')
+const runSequence = require('run-sequence')
+const batch = require('gulp-batch')
+
+let isWatching = false
 
 gulp
-  .task('default', () => {
+  .task('default', ['test'], () => {
+
+  })
+
+gulp
+  .task('dev', () => {
+    runSequence('build', 'test')
+    isWatching = true
+    gulp.watch('**/*.js', () => {
+      runSequence('build', 'test')
+    })
+  })
+
+gulp
+  .task('build', () => {
+    return new Promise((resolve) => {
+      console.log(`Building`)
+      setTimeout(() => resolve(), 1000)
+    })
+  })
+
+gulp
+  .task('test', () => {
+    return gulp.src('test/**/*.js')
+      .pipe(mocha())
+      .once('error', function() {
+        if (isWatching) {
+          // Emitting `end` event instead so gulp will not kill process.
+          this.emit('end')
+        } else {
+          process.exit(1)
+        }
+      })
   })
